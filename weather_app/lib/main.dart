1af89ad4b289ae521d5e07f04600945b330da3ae
DGNuwan <dgnuwanthilakarathna@gmail.com>
2025-04-15 02:36:22 +0530

    feat: add Weather Carousel layout to home screen

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,44 +1,166 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: weatherApp(),
-  ));
-}
 
-class weatherApp extends StatefulWidget {
-  const weatherApp({super.key});
 
-  @override
-  State<weatherApp> createState() => _weatherAppState();
-}
+void main() => runApp(const Weatherapp());
 
-class _weatherAppState extends State<weatherApp> {
+class Weatherapp extends StatelessWidget {
+  const Weatherapp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather App'),
-      ),
-      body: Center(
-        child: Column(
-         
-          children: [
-
-            Card(
-              child: Column(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.cast),
+          title: const Text('Weather App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text('City Name',style:TextStyle(fontSize:18,fontWeight: FontWeight.bold),),
-                  const Text('25°C',style:TextStyle(fontSize:50,fontWeight: FontWeight.bold),),
-                  const Text('Weather Condition',style:TextStyle(fontSize:18,fontWeight: FontWeight.bold),),
+                  SizedBox(
+                    height: 165,
+                    width: 165,
+                    child: weatercard(),
+                  ),
                 ],
               ),
-            ),
-            
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 250,
+                      child: Carousel(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class weatercard extends StatefulWidget {
+  const weatercard({super.key});
+
+  @override
+  State<weatercard> createState() => _weatercardState();
+}
+
+class _weatercardState extends State<weatercard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('City Name',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text('25°C',
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+              Text('Weather Condition',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Carousel extends StatefulWidget {
+  const Carousel({super.key});
+
+  @override
+  State<Carousel> createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  final CarouselController controller = CarouselController(initialItem: 1);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   // final double height = MediaQuery.sizeOf(context).height;
+
+    return ListView(
+      children: <Widget>[
+        const SizedBox(height: 20),
+        ConstrainedBox(
+          constraints: const BoxConstraints(maxHeight: 200),
+          child: CarouselView.weighted(
+            flexWeights: const <int>[3, 3, 3, 2, 1],
+            consumeMaxWeight: false,
+            children: CardInfo.values.map((CardInfo info) {
+              return ColoredBox(
+                color: info.backgroundColor,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(info.icon, color: info.color, size: 32.0),
+                      Text(
+                        info.label,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.clip,
+                        softWrap: false,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+enum CardInfo {
+  camera('Cameras', Icons.video_call, Color(0xff2354C7), Color(0xffECEFFD)),
+  lighting('Lighting', Icons.lightbulb, Color(0xff806C2A), Color(0xffFAEEDF)),
+  climate('Climate', Icons.thermostat, Color(0xffA44D2A), Color(0xffFAEDE7)),
+  wifi('Wifi', Icons.wifi, Color(0xff417345), Color(0xffE5F4E0)),
+  media('Media', Icons.library_music, Color(0xff2556C8), Color(0xffECEFFD)),
+  security(
+    'Security',
+    Icons.crisis_alert,
+    Color(0xff794C01),
+    Color(0xffFAEEDF),
+  ),
+  safety(
+    'Safety',
+    Icons.medical_services,
+    Color(0xff2251C5),
+    Color(0xffECEFFD),
+  ),
+  more('', Icons.add, Color(0xff201D1C), Color(0xffE3DFD8));
+
+  const CardInfo(this.label, this.icon, this.color, this.backgroundColor);
+  final String label;
+  final IconData icon;
+  final Color color;
+  final Color backgroundColor;
 }
